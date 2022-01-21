0758d996a11d7a18acad6f801749032f435ba3ac
GrimFlash <65523164+GrimFlash@users.noreply.github.com>
2022-01-20 19:58:50 -0500

    Update steam forum link

--- src/Core/info.h
+++ src/Core/info.h
@@ -7,5 +7,5 @@
 
 // Links
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
-#define MOD_LINK_FORUM L"https://steamcommunity.com/app/586140/discussions/0/3201496051820553074/"
+#define MOD_LINK_FORUM L"https://steamcommunity.com/app/586140/discussions/0/3195868146163014015/"
 #define MOD_LINK_GITHUB L"https://github.com/GrimFlash/BBCF-Improvement-Mod"
