c4ce973467e728ad618a4cd9b17c86fd95170b9d
philippejaram <127700219+libreofficecalc@users.noreply.github.com>
2023-10-14 17:29:21 -0300

    version bump

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,5 +1,5 @@
 #pragma once
-#define MOD_VERSION "v3.0540"
+#define MOD_VERSION "v3.0550"
 #define MOD_VERSION_NUM	MOD_VERSION " BO BLEIS edition"
 #define MOD_WINDOW_TITLE "BBCF Improvement Mod BO BLEIS"
 
